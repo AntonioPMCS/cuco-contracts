100d330a355edd4cac8849af84fa0fbc8907e330
AntonioPMCS <apmcarmosilva@ist.utl.pt>
2025-03-06 19:07:31 -0300

    First commit

---
+++ CuCoBlockchain.sol
@@ -0,0 +1,128 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CuCoBlockchain is Ownable {
+    
+    struct Customer {
+        uint256 id;
+        uint256 parentId;
+        uint256[] childIds;
+        address[] devices;
+        mapping(address => bool) authorizedUsers;  // Users with control over this customer
+        bool exists;
+    }
+
+    mapping(uint256 => Customer) public customers;
+    uint256 public customerCount = 0;
+
+    event CustomerCreated(uint256 customerId, uint256 parentId);
+    event DeviceAdded(uint256 customerId, address device);
+    event UserAdded(uint256 customerId, address user);
+
+    constructor() Ownable(msg.sender) {
+        // Create Root customer
+        customers[0].id = customerCount;
+        customers[0].parentId = 0;
+        customers[0].exists = true;
+        customers[0].authorizedUsers[msg.sender] = true;
+        emit CustomerCreated(customerCount, 0);
+        emit UserAdded(customerCount, msg.sender);
+    }
+
+    /**
+     * Create a new customer under a parent customer and assign an initial admin user.
+     */
+    function createCustomer(uint256 parentId, address adminUser) public onlyOwner returns (uint256) {
+        require(customers[parentId].exists, "Parent customer does not exist");
+
+        customerCount++;
+        customers[customerCount].id = customerCount;
+        customers[customerCount].parentId = parentId;
+        customers[customerCount].exists = true;
+        customers[customerCount].authorizedUsers[adminUser] = true;
+
+        customers[parentId].childIds.push(customerCount);
+
+        emit CustomerCreated(customerCount, parentId);
+        emit UserAdded(customerCount, adminUser);
+        return customerCount;
+    }
+
+    /**
+     * Assign a user to a customer.
+     */
+    function addUserToCustomer(uint256 customerId, address user) public {
+        require(customers[customerId].exists, "Customer does not exist");
+        require(_hasAccess(msg.sender, customerId), "Unauthorized");
+
+        customers[customerId].authorizedUsers[user] = true;
+        emit UserAdded(customerId, user);
+    }
+
+    /**
+     * Assign a device to a customer.
+     */
+    function addDeviceToCustomer(uint256 customerId, address device) public {
+        require(customers[customerId].exists, "Customer does not exist");
+        require(_hasAccess(msg.sender, customerId), "Unauthorized");
+
+        customers[customerId].devices.push(device);
+        emit DeviceAdded(customerId, device);
+    }
+
+    /**
+     * Get all devices managed by a customer (including child customers).
+     */
+    function getDevicesUnderCustomer(uint256 customerId) public view returns (address[] memory) {
+        require(customers[customerId].exists, "Customer does not exist");
+        require(_hasAccess(msg.sender, customerId), "Unauthorized");
+
+        return _gatherDevices(customerId);
+    }
+
+    /**
+     * Recursively gathers all devices from customer and child customers.
+     */
+    function _gatherDevices(uint256 customerId) internal view returns (address[] memory) {
+        uint256 totalDevices = _countDevices(customerId);
+        address[] memory result = new address[](totalDevices);
+        uint256 index = 0;
+
+        return _collectDevices(customerId, result, index);
+    }
+
+    function _countDevices(uint256 customerId) internal view returns (uint256 count) {
+        count += customers[customerId].devices.length;
+
+        for (uint256 i = 0; i < customers[customerId].childIds.length; i++) {
+            count += _countDevices(customers[customerId].childIds[i]);
+        }
+    }
+
+    function _collectDevices(uint256 customerId, address[] memory result, uint256 index) internal view returns (address[] memory) {
+        for (uint256 i = 0; i < customers[customerId].devices.length; i++) {
+            result[index++] = customers[customerId].devices[i];
+        }
+
+        for (uint256 i = 0; i < customers[customerId].childIds.length; i++) {
+            result = _collectDevices(customers[customerId].childIds[i], result, index);
+        }
+
+        return result;
+    }
+
+    /**
+     * Check if a user has access to a given customer or any of its parent customers.
+     */
+    function _hasAccess(address user, uint256 customerId) internal view returns (bool) {
+        while (customerId != 0) {
+            if (customers[customerId].authorizedUsers[user]) {
+                return true;
+            }
+            customerId = customers[customerId].parentId;
+        }
+        return false;
+    }
+}
---
+++ tests/CuCoBlockchain_test.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: GPL-3.0
+        
+pragma solidity >=0.4.22 <0.9.0;
+
+// This import is automatically injected by Remix
+import "remix_tests.sol"; 
+
+// This import is required to use custom transaction context
+// Although it may fail compilation in 'Solidity Compiler' plugin
+// But it will work fine in 'Solidity Unit Testing' plugin
+import "remix_accounts.sol";
+import "../CucoBlockchain.sol";
+import "hardhat/console.sol";
+
+// Using inheritance in testing is needed to have msg.sender and msg.value work as expected
+// When using inheritance, it is not needed to instantiate the contract
+contract CuCoBlockchainTest is CuCoBlockchain {
+
+    CuCoBlockchain cuco;
+    /// Define variables referring to different accounts
+    address acc0;
+    address acc1;
+
+    function beforeAll() public {
+        // <instantiate contract>
+        acc0 = TestsAccounts.getAccount(0);
+        acc1 = TestsAccounts.getAccount(1);
+    }
+
+    /// #sender: account-0
+    function checkDeploymentSuccess() public {
+        Assert.equal(owner(), acc0, 'Owner should be deployer');
+    }
+
+    function createCustomerSuccess() public {
+        createCustomer(0, acc1);
+        Assert.ok(customers[1].exists, "Customer does not exist");
+        Assert.equal(customers[1].id, 1, "Customer Id should be 1");
+        Assert.equal(customers[1].parentId, 0, "Customer parent id should be 0"); 
+        Assert.equal(customers[1].authorizedUsers[acc1], true, "acc1 should be authorized user");
+        Assert.equal(customers[1].childIds.length, 0,"New customer should start with 0 chidlren");
+        Assert.equal(customers[0].childIds[0], 1, "Root customer should have one child");
+    }
+
+    function createCustomerFail() public {
+        Assert.ok(!(customers[2].exists), "Customer exists but it should not");
+    }
+
+    function createCustomerOnChild() public {
+        
+    }
+
+    function checkSuccess() public {
+        // Use 'Assert' methods: https://remix-ide.readthedocs.io/en/latest/assert_library.html
+        Assert.ok(2 == 2, 'should be true');
+        Assert.greaterThan(uint(2), uint(1), "2 should be greater than to 1");
+        Assert.lesserThan(uint(2), uint(3), "2 should be lesser than to 3");
+    }
+
+    function checkSuccess2() public pure returns (bool) {
+        // Use the return value (true or false) to test the contract
+        return true;
+    }
+    
+    function checkFailure() public {
+        Assert.notEqual(uint(1), uint(1), "1 should not be equal to 1");
+    }
+
+    /// Custom Transaction Context: https://remix-ide.readthedocs.io/en/latest/unittesting.html#customization
+    /// #sender: account-1
+    /// #value: 100
+    function checkSenderAndValue() public payable {
+        // account index varies 0-9, value is in wei
+        Assert.equal(msg.sender, TestsAccounts.getAccount(1), "Invalid sender");
+        Assert.equal(msg.value, 100, "Invalid value");
+    }
+}
+    
